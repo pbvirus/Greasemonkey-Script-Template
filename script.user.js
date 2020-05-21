b60f47ae0e6258351a03e7b50abccf767db9c19f
Prince Biswaranjan <39714581+pbvirus@users.noreply.github.com>
2020-05-21 17:22:36 +0530

    Added base script

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        <Script Name>
+// @namespace   https://github.com/pbvirus
+// @description <Script Description>
+// @author      Prince Biswaranjan
+// @homepage    <Homepage URL>
+// @version     1.0.0
+// @grant       none
+// @include     *
+// @run-at      document-end
+// ==/UserScript==
+
+"use strict";
