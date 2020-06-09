78550c6f2e065dee68d267c987b8d3f1d72b05ed
Prince Biswaranjan <39714581+pbvirus@users.noreply.github.com>
2020-06-09 16:39:36 +0530

    Added code for adding button by default

--- script.user.js
+++ script.user.js
@@ -11,3 +11,29 @@
 // ==/UserScript==
 
 "use strict";
+
+function addButton(text, onclick, cssObj) {
+    cssObj = cssObj || {position: 'absolute', top: '7%', left:'4%', 'z-index': 3};
+    let button = document.createElement('button'), btnStyle = button.style;
+    document.body.appendChild(button);
+    button.innerHTML = text;
+    button.onclick = onclick;
+    btnStyle.position = 'absolute';
+    Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key]);
+    return button;
+}
+
+window.addEventListener('load', () => {
+    let cssProp = {
+        position: 'fixed', 
+        top: '6%', 
+        right:'1%', 
+        'z-index': 10,
+        'background-color': 'red'
+    };
+    addButton('Remove Future Tasks',  dummy, cssProp);
+});
+
+function dummy(){
+
+}
